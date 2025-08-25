12cdd4593dd3740c222dd8c53305becd9f152808
Sohaib_Raheem <shanineymar07@gmail.com>
2025-08-25 15:27:47 +0500

    Update script.js with local storage

--- script.js
+++ script.js
@@ -1,58 +1,71 @@
-var todos_array = [];
+var todos_array = JSON.parse(localStorage.getItem("todos")) || [];
+var editIndex = -1;
 
-var editIndex=-1;
-function addTodo(){
-  var inputElement= document.getElementById("todo")
-  if(inputElement.value === ''){
-    alert("Please enter a task...")
-    return
-  }
-  todos_array.push(inputElement.value)
-  inputElement.value=""
-  showList()
+var addBtn = document.getElementById("addBtn");
+var updateBtn = document.getElementById("updateBtn");
+
+function saveToLocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos_array));
 }
 
-function showList(){
-  var listItems = document.getElementById("todo-list")
+function addTodo() {
+  var inputElement = document.getElementById("todo");
+  if (inputElement.value === "") {
+    alert("Please enter a task...");
+    return;
+  }
+  todos_array.push(inputElement.value);
+  inputElement.value = "";
+  saveToLocalStorage();
+  showList();
+}
 
-  listItems.innerHTML=""
-for( var i=0; i < todos_array.length; i++){
-listItems.innerHTML += `<li class="todo-item"><span class="check"></span>  ${todos_array[i]}   <div class="actions">
-          <button  onclick="editTodo(${i})" class="edit-btn" >✏️</button>
+function showList() {
+  var listItems = document.getElementById("todo-list");
+  listItems.innerHTML = "";
+  for (var i = 0; i < todos_array.length; i++) {
+    listItems.innerHTML += `
+      <li class="todo-item">
+        <span class="check"></span> ${todos_array[i]}
+        <div class="actions">
+          <button onclick="editTodo(${i})" class="edit-btn">✏️</button>
           <button onclick="deleteTodo(${i})" class="delete-btn">🗑️</button>
         </div>
-        </li>`;
-}}
-
-function deleteTodo(index){
-  todos_array.splice(index,1)
-  showList()
+      </li>`;
+  }
 }
 
-function editTodo(index){
-  var inputElement= document.getElementById("todo")
-   inputElement.value=todos_array[index]
-   editIndex = index;
-        
-        addBtn.style.display="none";
-        updateBtn.style.display="inline-block"
-        
-        
+function deleteTodo(index) {
+  todos_array.splice(index, 1);
+  saveToLocalStorage();
+  showList();
 }
 
-function updateTodo(){
-  var inputElement= document.getElementById("todo")
-  if(inputElement.value === ''){
-    alert("Please enter a task...")
-    return
+function editTodo(index) {
+  var inputElement = document.getElementById("todo");
+  inputElement.value = todos_array[index];
+  editIndex = index;
+
+  addBtn.style.display = "none";
+  updateBtn.style.display = "inline-block";
+}
+
+function updateTodo() {
+  var inputElement = document.getElementById("todo");
+  if (inputElement.value === "") {
+    alert("Please enter a task...");
+    return;
   }
   todos_array[editIndex] = inputElement.value;
-  inputElement.value=""
-  editIndex=-1;
-  
-  
-  addBtn.style.display="inline-block";
-  updateBtn.style.display="none"
-  
-  showList()
+  inputElement.value = "";
+  editIndex = -1;
+
+  addBtn.style.display = "inline-block";
+  updateBtn.style.display = "none";
+
+  saveToLocalStorage();
+  showList();
 }
+
+
+showList();
